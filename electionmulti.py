60dd042f4eb3ad13de0abaa779de5415863f50cf
Stephen Jackson <scj7t4@mst.edu>
2013-10-03 17:56:25 -0500

    Update electionmulti.py

--- electionmulti.py
+++ electionmulti.py
@@ -20,118 +20,98 @@ MAX_ATTEMPTS = 500
 MAX_CHECKS = 50
 TRIALS = 10
 
-
-"""
-for i in range(0,101):
-    s = 0.0
-    t = 50000
-    for j in range(t):
-        s += 1 if attempt_delivery(i) else 0
-"""
-
-class CommonStore(object):
+class Participant(object):
     def __init__(self,premerge_v=None):
+        self.AYC = False
+        self.AYC_response = []
+        self.remoteAYC = []
         self.invite = False
-        self.accept = False
-        self.invite_foreign = False
-        self.ready_foreign = False
-        self.leader = False
-        self.premerge_v = random.randint(PREMERGE_MIN,PREMERGE_MAX) if premerge_v == None else premerge_v
+		self.invite_response = [] # replaces self.accept.
+        self.remoteInvite = None
+        self.ready = False
+        self.remoteReady = None
+        self.remoteInvite = False
+        self.premerge_v = random.randint(50,150) if premerge_v == None else premerge_v
+		self.acceptingRemoteInvite = True
+		self.channels = {}
+	
         self.ayctimer = GLOBAL_TIMEOUT
         self.premerge = None
         self.peerwait = GLOBAL_TIMEOUT
         self.timeout = TIMEOUT_TIMEOUT
         self.ticks = 0
-        self.enable_premerge = False
-        self.ready = False
-        self.enable_ayctimer = False
 
+	def add_channel(self, peer, channel):
+		self.channels[peer] = channel
+		
     def tick(self):
-        # If a premerge timer has been set and hasn't expired, tick the timer
-        if self.premerge != None and self.premerge > 0 and not self.invite_foreign:
-            self.premerge -= 1
         #if you've sent and invite and you are the leader, and the timer for peers responding haven't expired, tick
-        if self.invite and self.leader and self.peerwait > 0:
+        if self.invite and not self.acceptingRemoteInvite and self.peerwait > 0:
             self.peerwait -= 1
+			
         #if you've sent an AYC and and it hasn't expired, tick the timer
-        if self.enable_ayctimer and self.ayctimer > 0:
+        if self.AYC and self.ayctimer > 0:
             self.ayctimer -= 1
-        if self.enable_premerge and self.ayctimer == 0 and self.premerge == None:
+			
+		# If a premerge timer has been set and hasn't expired, tick the timer
+		if self.premerge != None and self.premerge > 0 and not self.remoteInvite:
+            self.premerge -= 1
+			
+        if len(self.AYC_response) > 0 and self.ayctimer == 0 and self.premerge == None:
             #If you've recieved a response and the timer has expired, select a premerge
             self.premerge = self.premerge_v
+			
         #If you've recieved an invite but not the ready, tick down the timeout.
-        if self.timeout > 0 and self.invite_foreign and not self.leader and not self.ready_foreign:
+        if self.timeout > 0 and self.remoteInvite != None and self.acceptingRemoteInvite and not self.remoteReady:
             self.timeout -= 1
 
-    def finished(self):
-        if self.ready or self.ready_foreign:
-            return True
-        return False
-
-
-class Participant(object):
-    def __init__(self,common):
-        self.AYC = False
-        self.AYC_response = False
-        self.AYC_foreign = False
-        self.ready = False
-        self.accept_foreign = False
-        self.sent_invite = False
-        self.invite_from_this = False
-        self.ready_from_this = False
-        self.common = common 
-
-    def tick(self,protocol):
-        if self.AYC_response and self.common.enable_premerge == False:
-            #If you've recieved a response and the timer has expired, select a premerge
-            self.common.enable_premerge = True
-
         if not self.AYC:
-            protocol.send("AYC")
+			for peer in self.channels:
+				self.channels[peer].send("AYC")
             self.AYC = True
-            self.common.enable_ayctimer = True
 
-        if self.AYC_response and self.common.ayctimer == 0 and self.common.premerge == 0 and not self.common.invite_foreign:
-            self.common.leader = True
+        if len(self.AYC_response) > 0 and self.ayctimer == 0 and self.premerge == 0 and not self.remoteInvite and not self.invite:
+			for peer in self.channels:
+				if peer in self.AYC_response:
+					protocol.send("INVITE")
+            self.invite = True
+            self.acceptingRemoteInvite = False
 
-        if self.common.leader and not self.sent_invite:
-            protocol.send("INVITE")
-            self.common.invite = True
-            self.sent_invite = True
-
-        if self.accept_foreign and self.common.peerwait == 0 and not self.ready:
-            protocol.send("READY")
-            self.common.ready = True
-            self.ready = True   
- 
-    def recv(self,protocol,m):
+        if self.peerwait == 0 and not self.ready:
+			for peer in self.channels:
+				if peer in self.invite_response:
+					protocol.send("READY")
+            self.ready = True
+    
+    def recv(self,peer,m):
         (t,s,msg) = m
         if msg == "AYC":
-            self.AYC_foreign = True
-            protocol.send("AYC_R")
+            self.remote_AYC.append(peer)
+            self.channels[peer].send("AYC_R")
 
-        elif msg == "AYC_R" and self.common.ayctimer > 0:
-            self.AYC_response = True
+        elif msg == "AYC_R" and self.ayctimer > 0:
+            self.AYC_response.append(peer)
 
-        elif msg == "INVITE" and not self.common.invite_foreign and not self.common.leader:
-            self.common.invite_foreign = True
-            self.invite_from_this = True
-            if self.common.leader == False:
-                protocol.send("ACCEPT")
+        elif msg == "INVITE" and self.remoteInvite == None:
+            if self.acceptingRemoteInvite == True:
+				self.remoteInvite = peer
+                self.channels[peer].send("ACCEPT")
 
-        elif msg == "ACCEPT" and self.common.leader and self.common.peerwait > 0:
-            self.accept_foreign = True
+        elif msg == "ACCEPT" and not self.acceptingRemoteInvite and self.peerwait > 0:
+			self.invite_response.append(peer)
 
-        elif msg == "READY" and self.common.timeout > 0 and not self.common.ready_foreign and self.invite_from_this == True:
-            self.common.ready_foreign = True
-            self.ready_from_this = True
+        elif msg == "READY" and self.timeout > 0 and self.remoteReady == None and self.remoteInvite == peer:
+            self.remoteReady = True
 
     def aycfinished(self):
-        if self.AYC and self.AYC_response:
+        if self.AYC and len(self.AYC_response) > 0:
             return True
         return False
 
-
+    def finished(self):
+        if self.ready or self.remoteReady:
+            return True
+        return False
 
 def simulation(p):
     offset = 50
@@ -144,10 +124,10 @@ def simulation(p):
         if r == True:
             quicks += 1
         else:
-            if len(q) > 0 and max([len(x) for x in q]) == 1:
+            if max(q) == 1:
                 #print q
                 slows += 1
-            elif len(q) > 0:
+            else:
                 break
         bad += 1
 
@@ -155,52 +135,56 @@ def simulation(p):
 
 
 def check(p, offset):
-    # The values stored globably by each processes (Shared betweeen all channels)
-    commons = [ CommonStore() for c in range(PARTICIPANTS) ]
-    # A helper for the iterators
-    iterthing = []
+
     # The message channels (one for each pairing of nodes, (i,j)
-    channel = {}
+    channels = {}
+	peers = {}
 
     relmap = reliability(p)
     # For each pair of participiants, create a unidirectional channel and protocol between them.
+	for i in range(PARTICIPANTS):
+		peer[i] = Participant()
+	
     for i in range(PARTICIPANTS):
         for j in range(PARTICIPANTS):
             if i == j:
                 continue
-            iterthing.append( (i,j) )
-            if i not in channel:
-                channel[i] = {}
-            channel[i][j] = ( Participant(commons[i]), SUC(relmap[(i,j)]) )
+            if i not in channels:
+                channels[i] = {}
+            channels[i][j] = SUC(relmap[(i,j)]) 
+			peer[i].add_channel(j, channels[i][j])
 
     # Attempt Counter
     s = 0
     # While the maximum number of attempts has not been reached and not all nodes are ready:
-    while s < MAX_ATTEMPTS:
-
-        for c in commons:
-            c.tick()
-
+    while (s < MAX_ATTEMPTS and ( not all([peer.finished() for peer in peers]) ) ):
+	
         # Advance each participants clock
-        for (i,j) in iterthing:
-            channel[i][j][0].tick(channel[i][j][1])
+        for peer in peers:
+			peers[peer].tick()
         
         # For each pair of nodes, see if any messages can be recieved
         mb = {}
-        for (i,j) in iterthing:
-            if (j,i) not in mb:
-                mb[(j,i)] = []
-            mb[(j,i)] += [ m for m in channel[i][j][1].tick() ]
+        for i in range(PARTICIPANTS):
+			for j in range(PARTICIPANTS):
+				if i == j:
+					continue:
+				if (j,i) not in mb: # Message from i, to j.
+					mb[(j,i)] = []
+				mb[(j,i)] += [ m for m in channels[i][j].tick() ]
 
 
         #for (k,v) in mb.iteritems():
         #    print "{} : {}".format(k,v)
 
         # For each pair of nodes, see if the recieved message is accepted by the protocol and delivered to j.
-        for (i,j) in iterthing:
-            for m in mb[(i,j)]:
-                if channel[i][j][1].recv(m):
-                    channel[i][j][0].recv(channel[i][j][1],m)
+        for i in range(PARTICIPANTS):
+			for j in range(PARTICIPANTS):
+				if i == j:
+					continue:
+				for m in mb[(j,i)]:
+					if channel[j][i].recv(m):
+						peer[j].recv(i,m)
 
         # Mark an attempt.
         s += 1
@@ -208,22 +192,20 @@ def check(p, offset):
     r = []
     for i in range(PARTICIPANTS):
         q = []
-        if commons[i].leader == True and commons[i].finished() == True:
+        if peer[i].invite == True:
             q.append(i)
             for j in range(PARTICIPANTS):
-                if not commons[j].finished():
-                    continue
                 if i == j:
                     continue
-                if channel[j][i][0].ready_from_this and channel[j][i][0].common.finished():
-                    q.append(j)
+				if peer[j].remoteInvite == i and peer[j].remoteReady == True:
+					q.append(j)
             r.append(tuple(q))
     
     quick = True
     #evaluate if the failure mode is a slow failure or a fast one
     #A slow failure is when all communication channels get past the AYC phase
     #But fail to form a group.
-    if any([ channel[i][j][0].aycfinished() for (i,j) in iterthing ]):
+    if any([ peer.aycfinished() for peer in peers ]):
         quick = False    
     
     return (quick,r)
@@ -253,4 +235,3 @@ for p in range(0,100):
     #Print the counts of the different types of groups formed;
     for (k,v) in d.iteritems():
         print "#{}: {}".format(k,v)
-
